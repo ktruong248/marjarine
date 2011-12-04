75c2dd39fb41874038b8d24e36deb5ce95295a23
Sebastian Wittenkamp <a.sebastian.w@gmail.com>
2011-12-04 00:16:12 -0800

    First checkin, playing around.

---
+++ test/example/test/core.clj
@@ -0,0 +1,9 @@
+(ns example.test.core
+  (:use [example.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is true "No tests have been written."))
+
+(deftest ishouldfail
+  (fail "booya"))
